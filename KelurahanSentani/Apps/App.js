2d0b728fb6105ee8c9d70e2d918cacef047012fb
D-D <D-D@D-D>
2017-08-23 19:43:27 +0900

    #First Commit f ile

---
+++ KelurahanSentani/Apps/App.js
@@ -0,0 +1,56 @@
+﻿angular.module('app', ['ngRoute'])
+.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl:"main.htm",
+        })
+        .when("/rw", {
+            templateUrl: "rw.htm",
+        })
+        .when("/view", {
+            templateUrl: "../Apps/Views/view.html",
+    ;
+})
+
+.controller("RedController", function ($scope) {
+
+    $scope.RW= [];
+
+    $scope.Init=function()
+    {
+        var rw ={};
+        rw.Nama="I";
+        rw.RT=[];
+        
+        var rt1 = {};
+        rt1.Nama="1";
+
+        var rt2 = {};
+        rt2.Nama="2";
+
+
+        rw.RT.push(rt1);
+        rw.RT.push(rt2);
+
+        $scope.RW.push(rw);
+        $scope.RW.push({ 'Nama': 'II' });
+
+    }
+
+    $scope.ShowName=function(model)
+    {
+        $scope.RW.push(model);
+    }
+
+   
+
+
+})
+
+
+
+
+
+
+
+    ;
