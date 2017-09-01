6c0ff4dc3af48df6ad9a14473b78ed8f22e19c7a
ocph23 <ocph23@hotmail.com>
2017-09-01 13:06:14 +0900

    #01092017-2

--- KelurahanSentani/Controllers/AccountController.cs
+++ KelurahanSentani/Controllers/AccountController.cs
@@ -172,6 +172,8 @@ namespace KelurahanSentani.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
+                    await UserManager.AddToRoleAsync(user.Id, "Administrator");
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
