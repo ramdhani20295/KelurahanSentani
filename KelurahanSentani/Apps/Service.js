f08d59249342be6cc52db6d0580c8dba213ff0ac
ocph23 <ocph23@hotmail.com>
2017-09-04 04:06:24 +0900

    #ocph 03092017-End

--- KelurahanSentani/Apps/Service.js
+++ KelurahanSentani/Apps/Service.js
@@ -152,19 +152,20 @@
 
     .factory("StrukturKelurahanService", function ($http, $q, BaseUrl, Helpers) {
         var service = {};
-        service.isInstant = false;
-        service.collection = [];
+        var isInstant = false;
+        var collection = [];
+
         service.source = function () {
             deferred = $q.defer();
-            if (!service.isInstant) {
+            if (!isInstant) {
                 $http({
                     method: 'GET',
                     url: BaseUrl.URL + "/api/StrukturKelurahan/getrw",
                 }).then(function (data) {
                     // With the data succesfully returned, we can resolve promise and we can access it in controller
-                    service.collection = data;
-                    deferred.resolve(service.collection);
-                    service.isInstant = true;
+                    collection = data;
+                    deferred.resolve(collection);
+                    isInstant = true;
 
                 }, function (error) {
 
@@ -173,36 +174,28 @@
                 });
 
             } else {
-                deferred.resolve(service.collection);
+                deferred.resolve(collection);
             }
 
             return deferred.promise;
         };
 
-        service.Tambah = function(model)
+        service.Insert = function(model)
         {
             deferred = $q.defer();
-            if (!service.isInstant) {
-                $http({
-                    method: 'post',
-                    url: BaseUrl.URL + "/api/StrukturKelurahan/postrw",
-                    data: model
-                }).then(function (data) {
+            $http({
+                method: 'POST',
+                url: BaseUrl.URL + "/api/StrukturKelurahan/postrw",
+                data: model
+            }).then(function (data) {
+                collection.data.push(data);
+                deferred.resolve(data);
+               
+            }, function (error) {
 
-
-                    service.collection.push(data);
-                    deferred.resolve(service.collection);
-                    service.isInstant = true;
-
-                }, function (error) {
-
-                    alert(Helpers.getMessage(error.status, error.data.Message));
-                    // deferred.reject(error);
-                });
-
-            } else {
-                deferred.resolve(service.collection);
-            }
+                alert(Helpers.getMessage(error.status, error.data.Message));
+                // deferred.reject(error);
+            });
 
             return deferred.promise;
         }
